53e891c9408d0aaf53d3dc1819e76c9ee2e67ca7
Emil Valiev <evaliev90@gmail.com>
2024-07-08 15:41:13 +0500

    add vercel.json

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +26,6 @@ const router = createRouter({
       ]
     }
   ]
-})
+});
 
-export default router
+export default router;
